e4b22dde0deb6d440122798b5702232bb5cc78f9
NectoT <44334753+NectoT@users.noreply.github.com>
2020-09-10 14:46:42 +0300

    Update Human.java

--- Praktika3/Human.java
+++ Praktika3/Human.java
@@ -1,45 +1,105 @@
 package Praktika3;
 
-class Rib {
-    Rib(int age, String color) {
-        this.age = age;
+class Head {
+    Head(double height, int iq, String color) {
+        this.height = height;
+        this.iq = iq;
         this.color = color;
     }
 
-    Rib(int age) {
+    private int iq;
+    private double height;
+    private String color;
+
+    public double getHeight() {
+        return height;
+    }
+
+    public int getIq() {
+        return iq;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void think() {
+        if (Math.random() > 0.5) {
+            iq++;
+        }
+        else {
+            iq--;
+        }
+    }
+}
+
+class Leg {
+    Leg(double height, String color) {
+        this.height = height;
+        this.color = color;
+    }
+
+    private double height;
+    private String color;
+
+    public double getHeight() {
+        return height;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
+
+class Hand {
+    Hand(double height, String color) {
+        this.height = height;
+        this.color = color;
+    }
+
+    private double height;
+    private String color;
+
+    public double getHeight() {
+        return height;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
+
+public class Human {
+
+    Human(double height, int age, int iq, String color) {
+        rightLeg = new Leg(height / 2, color);
+        leftLeg = new Leg(height / 2, color);
+        rightHand = new Hand(height / 3, color);
+        leftHand = new Hand(height / 3, color);
+        head = new Head(height / 6, iq, color);
         this.age = age;
     }
 
-    protected int age;
-    protected String color = "White";
+    private Leg rightLeg;
+    private Leg leftLeg;
+    private Hand rightHand;
+    private Hand leftHand;
+    private Head head;
 
-    void growOlder() {
+    private int age;
+
+    public double getApproximateHeight() {
+        return rightLeg.getHeight() + rightHand.getHeight() + head.getHeight();
+    }
+
+    public void live() {
+        head.think();
         age++;
     }
 
-}
-
-class Woman extends Rib {
-    Woman(int age, int height) {
-        super(age);
-        this.height = height;
+    public static void main(String[] args) { // for testing purposes
+        Human danya = new Human(140, 90, 90, "Olive");
+        danya.live();
+        System.out.println(danya.getApproximateHeight());
     }
-
-    Woman(int age, int height, String color) {
-        super(age, color);
-        this.height = height;
-    }
-
-    protected int height;
-}
-
-public class Human { // basically a Main class
-
-    public static void main(String[] args) {
-        Rib rib = new Rib(0);
-        rib.growOlder();
-        Woman eve = new Woman(0, 170, "Black");
-        eve.growOlder();
-    }
-
 }
